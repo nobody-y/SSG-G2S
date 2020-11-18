9a1e178868aacdecf0613ec58f1cda404728885b
zgkk123456@vip.qq.com <nobody-y>
2020-11-18 14:41:17 +0800

    first commit

--- misc/count_predicate_wiki.py
+++
@@ -1,8 +0,0 @@
-import wikipedia
-import json
-import numpy as np
-VG_dict = json.load(open('/mnt/hdd2/***/datasets/visual_genome/data/genome/VG-SGG-dicts.json','r'))
-pred_list = VG_dict['predicate_to_idx']
-pred_count_wikipedia_all = {}
-for i in pred_list:
-    pred_count_wikipedia_all[i] = len(wikipedia.search(i,results=np.inf))
